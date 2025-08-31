7e9435c0e917c6754b83f14b8afe615ffb407d80
simplywhiterobot <s@s.comm>
2025-09-01 01:25:24 +0530

    update

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace with your Apps Script Web App URL
-window.BACKEND_URL = 'https://script.google.com/macros/s/AKfycbxR70wIk1qNCh23ochCw_o03jLN2ZNdplxHc_jlaaghEPgw857N-_BQ8irWlmrzYZGr8g/exec';
+window.BACKEND_URL = 'https://script.google.com/macros/s/AKfycbyySQOse4K5YfYfGkmmT7j7HPpq0ilEXjbacwrVbDiDjZFXRjmX2Moqj8CDiBZxuYZA/exec';
